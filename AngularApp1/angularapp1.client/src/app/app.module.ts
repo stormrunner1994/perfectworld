b03322faddb0281a0fabac5f4fbe95faaef08800
Runner <Runner@LAPTOP-I5IDQFV5>
2025-07-26 22:11:32 +0200

    bugfix

--- AngularApp1/angularapp1.client/src/app/app.module.ts
+++ AngularApp1/angularapp1.client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,10 +9,10 @@ import { TopicInputComponent } from './topic-input/topic-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,  
   ],
   imports: [
-    BrowserModule, HttpClientModule, TopicInputComponent,
+    BrowserModule, HttpClientModule, TopicInputComponent, FormsModule ,
     AppRoutingModule
   ],
   providers: [],
